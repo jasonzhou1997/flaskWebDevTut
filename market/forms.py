6f754d47789b3a2bb950d9dd667cdcec06541444
jasonzhou1997 <60407587+jasonzhou1997@users.noreply.github.com>
2022-04-10 21:39:21 -0400

    Finished the validations tutorial

--- market/forms.py
+++ market/forms.py
@@ -1,14 +1,28 @@
 from ast import Pass
+import email
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+from market.models import User
 
 #create registered forms
 
 class RegisterForm(FlaskForm):
-    username = StringField(label="User Name: ")
-    email_address = StringField(label="Email Address: ")
-    password1 = PasswordField(label = "Password: ")
-    password2 = PasswordField(label = "Confirm Password: ")
+    #check if given username is already existing inside database
+    def validate_username(self, username_to_check):
+        user = User.query.filter_by(username = username_to_check.data).first()
+        if user:
+            raise ValidationError("Username already exits! Please try a different username")
+        
+    def validate_email_address(self, emailaddress_to_check):
+        email_address = User.query.filter_by(email_address = emailaddress_to_check.data).first()
+        if email_address:
+            raise ValidationError("Email Address already exists! Please try a different email address")
+        
+    username = StringField(label="User Name: ", validators=[Length(min=3, max=15), DataRequired()])
+    email_address = StringField(label="Email Address: ", validators=[Email(), DataRequired()])
+    password1 = PasswordField(label = "Password: ", validators = [Length(min=6), DataRequired()])
+    password2 = PasswordField(label = "Confirm Password: ", validators=[EqualTo("password1"), DataRequired()])
     submit = SubmitField(label = "Create Account")
     
     
--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,8 @@
 from market import app
-from flask import render_template
-from market.models import Item
+from flask import render_template, redirect, url_for, flash, get_flashed_messages
+from market.models import Item, User
 from market.forms import RegisterForm
+from market import db
 
 @app.route("/")
 @app.route("/home")
@@ -14,8 +15,21 @@ def market_page():
     items = Item.query.all()
     return render_template("market.html", items = items)
 
-@app.route("/register")
+@app.route("/register", methods = ["GET","POST"])
 def register_page():
     form = RegisterForm()
+    # flask form validation
+    if form.validate_on_submit():
+        user_to_create = User(username = form.username.data,
+                              email_address = form.email_address.data,
+                              password_hash = form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for("market_page"))
+    #If there are not errors from the validations
+    if form.errors != {}:
+        for err_msg in form.errors.values():
+            flash(f"There was an error with creating a user: {err_msg}", category="danger")
+       
     return render_template("register.html", form = form)
     
