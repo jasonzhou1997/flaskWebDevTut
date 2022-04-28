0744778cc4999c27a8fbfc76cff6162cb236ffe9
jasonzhou1997 <60407587+jasonzhou1997@users.noreply.github.com>
2022-04-28 12:43:02 -0400

    Finished Login/out Tutorial

--- market/__init__.py
+++ market/__init__.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db"
 app.config["SECRET_KEY"] = "b7ef285e86b0afaf33b15b80"
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
 
 from market import routes
